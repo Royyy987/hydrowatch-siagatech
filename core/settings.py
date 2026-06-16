ebc36c75275feb248af67bbc5faeab9560f0ee61
Muhammad Rizky Raditya <rizkyraditya700@gmail.com>
2026-06-16 18:42:34 +0800

    fix: downgrade whitenoise storage to prevent missing static files

--- core/settings.py
+++ core/settings.py
@@ -153,6 +153,7 @@ USE_TZ = True
 # --- STATIC & MEDIA FILES ---
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
