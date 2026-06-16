a4ded16a3be176c64b9ce476198fcf2537fd0ab3
Muhammad Rizky Raditya <rizkyraditya700@gmail.com>
2026-06-16 18:50:05 +0800

    fix: update static URL path and downgrade whitenoise storage

--- core/settings.py
+++ core/settings.py
@@ -151,8 +151,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 # --- STATIC & MEDIA FILES ---
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
